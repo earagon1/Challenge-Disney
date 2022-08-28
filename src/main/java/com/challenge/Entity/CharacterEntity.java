46abd688c37a3bfdea3be24ecb2cce08449608ac
earagon1 <eve_aragon@hotmail.com>
2022-08-27 23:55:49 -0300

    agregando relaciones manytoone

--- src/main/java/com/challenge/Entity/CharacterEntity.java
+++ src/main/java/com/challenge/Entity/CharacterEntity.java
@@ -1,11 +1,15 @@
 package com.challenge.Entity;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -44,5 +48,11 @@ public class CharacterEntity {
 	@Column(name = "character_image")
 	private String characterImage;
 
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "movie_id", insertable = false, updatable = false)
+	private MovieEntity movie;
+	
+	@Column(name = "movie_id", nullable = false)
+	private Long movieId;
 
 }
--- src/main/java/com/challenge/Entity/MovieEntity.java
+++ src/main/java/com/challenge/Entity/MovieEntity.java
@@ -4,13 +4,17 @@ import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -48,5 +52,11 @@ public class MovieEntity {
 	@Column @Enumerated(EnumType.STRING)
 	private Qualification qualification;
 	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "genre_id", insertable = false, updatable = false)
+	private GenreEntity genre;
+	
+	@Column(name = "genre_id", nullable = false)
+	private Long genreId;
 	
 }
