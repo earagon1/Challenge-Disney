a8ee79e18e363eaaefe387c1ad797bb21a51e49d
earagon1 <eve_aragon@hotmail.com>
2022-09-08 20:34:12 -0300

    userDTO

---
+++ src/main/java/com/challenge/DTO/UserDTO.java
@@ -0,0 +1,13 @@
+package com.challenge.DTO;
+
+import lombok.Data;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+@Data
+public class UserDTO
+{
+    @Email(message="Username must be an email")
+    private String userName;
+    @Size(min =0)
+    private String password;
+}
